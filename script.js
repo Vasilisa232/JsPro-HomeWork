62013f1ae5facb11fdbc954f02e35ba312852283
Vasilisa23 <vasilisadeni7@yandex.ru>
2022-06-25 19:25:56 +0300

    v. 1.0.6

--- script.js
+++ script.js
@@ -7,26 +7,63 @@ function service(url) {
   .then((res) => res.json());
 }
 
+function init() {
+  Vue.component('basket', {
+    template: `
+  <div class="fixed-area">
+    <div class="basket-card">
+      <div class="basket-card__header">
+        <h1 class="basket-card__header__title">basket-card</h1>
+        <div class="basket-card__header__delete-icon" @click="$emit('close')">
+        </div>
+      </div>
+      <div class="basket-card__content">content
+       {{calculatePrice}}
+      </div>
+    </div>
+  </div>`
+  })
 
-window.onload = () => {
+  Vue.component('custom-button', {
+    template: 
+    `<button class="search-button" type="button" v-on:click="$emit('click')">
+    <slot></slot>
+    </button>`
+  }) 
+  Vue.component('good', {
+    props: [
+      'item'
+    ],
+    template:
+    `<div class="goods-item">
+      <h3>{{ item.product_name}} </h3>
+      <p>{{ item.price }}</p>
+    </div>`
+  })
+
+  Vue.component('custom-search', {
+    template:`
+      <input type="text" class="goods-search" @input="$emit('input', $event.target.value)"/>
+    `
+  })
+
   const app = new Vue({
     el: '#root',
     data: {
       items: [],
-      filteredItems: [],
       searchValue: '',
       isVisibleCart: false,
     },
-
     methods: {
       setVisibleCart() {
         this.isVisibleCart = !this.isVisibleCart
-      }
+      },
+
     },
     mounted() {
       service(GET_GOODS_ITEMS).then((data) => {
         this.items = data;
-        this.filteredItems =data;
+        this.filteredItems = data;
         return data;
       })
     },
@@ -37,11 +74,12 @@ window.onload = () => {
         }, 0)
       },
       filteredItems() {
-          return this.items.filter(({ product_name }) => {
-            return product_name.match(new RegExp(this.searchValue, 'gui'))
-          })
-        
+        return this.items.filter(({ product_name }) => {
+          return product_name.match(new RegExp(this.searchValue, 'gui'))
+        })
       }
-    }
+    },
   })
+
 }
+window.onload = init;
