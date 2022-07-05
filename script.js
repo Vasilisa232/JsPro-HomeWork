969f8278a756f2e4951dd135363543efa782925a
Vasilisa23 <vasilisadeni7@yandex.ru>
2022-07-05 14:53:58 +0300

    sorry

--- script.js
+++ script.js
@@ -7,16 +7,71 @@ function service(url) {
   .then((res) => res.json());
 }
 
+Vue.component('basket', {
+  template:`
+  <div class="fixed-area">
+      <div class="basket-card">
+        <div class="basket-card__header">
+          <h1 class="basket-card__header__title">basket-card</h1>
+          <div class="basket-card__header__delete-icon"
+          @click="$emit('close')"></div>
+        </div>
+        <div class="basket-card__content">content
+        </div>
+      </div>
+    </div>
+  `
+})
+
+Vue.component('custom-button', {
+  template: `
+  <button class="search-button" type="button" v-on:click="$emit('click')">
+  <slot></slot>
+  </button>
+  `
+})
+
+Vue.component('custom-input', {
+  props: ['value'],
+  template: `
+    <input
+      v-bind:value="value"
+      v-on:input="$emit('input', $event.target.value)"
+    >
+  `
+})
+
+Vue.component('good', {
+  props: [
+    'item'
+  ],
+  template: `
+  <div class="goods-item">
+    <h3>{{ item.product_name}} </h3>
+    <p>{{ item.price }}</p>
+  </div>
+  `
+})
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Привет, Vue!'
+  }
+})
+
+
+
 
 window.onload = () => {
-  const app = new Vue({
+  const root = new Vue({
     el: '#root',
     data: {
       items: [],
-      filteredItems: [],
       searchValue: '',
       isVisibleCart: false,
     },
+    
 
     methods: {
       setVisibleCart() {
@@ -26,7 +81,6 @@ window.onload = () => {
     mounted() {
       service(GET_GOODS_ITEMS).then((data) => {
         this.items = data;
-        this.filteredItems =data;
         return data;
       })
     },
@@ -36,6 +90,7 @@ window.onload = () => {
           return prev + price;
         }, 0)
       },
+
       filteredItems() {
           return this.items.filter(({ product_name }) => {
             return product_name.match(new RegExp(this.searchValue, 'gui'))
